371b41c23fa23a2eae3f9236aea89dd53e15c2ca
Bence Dányi <madbence@gmail.com>
2013-06-26 06:55:00 -0700

    test

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-setInterval(function() { console.log('Hello!')}, 1000);
+setInterval(function() { console.log('Hello!', JSON.stringify(process.env))}, 1000);
+var http=require('http');
+
+http.createServer(function(req, res) {
+	res.end(process.env['DEV_PORT']+' '+process.env['DEV_BRANCH']);
+}).listen(process.env['DEV_PORT']);
+console.log(process.env);
